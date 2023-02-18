2f183b782a4ed19ae08933d86343004e45d828c8
Shaped Sundew <16618209+Shapedsundew9@users.noreply.github.com>
2023-02-18 17:27:40 -0500

    Latest

---
+++ egp_worker/platform_info.py
@@ -0,0 +1,51 @@
+""" platform_info module."""
+from logging import Logger, NullHandler, getLogger
+from platform import (machine, platform, processor, python_version, release,
+                      system)
+from pprint import pformat
+from sys import exit as sys_exit
+from typing import Any
+
+from pypgtable.table import table
+from pypgtable.typing import TableConfigNorm
+
+from .platform_info_validator import platform_info_validator
+
+_logger: Logger = getLogger(__name__)
+_logger.addHandler(NullHandler())
+
+
+# TODO: Implemented EGPOps
+def _get_platform_info() -> dict[str, Any]:
+    # TODO: Replace these with an Erasmus benchmark.
+    # The metric needs to be stable on a system to 1 unit as it is used in the SHA256 signature to
+    # identify the platform.
+    performance: float = 1.0
+    return {
+        "machine": machine(),
+        "processor": processor(),
+        "platform": platform(),
+        "python_version": python_version(),
+        "system": system(),
+        "release": release(),
+        "EGPOps/s": performance
+    }
+
+
+# Add the platform info to the platform info table if it is not already there & return it
+def get_platform_info(table_config: TableConfigNorm) -> dict[str, Any]:
+    """Introspect the system and record it in the platform table as needed."""
+    platform_info: dict[str, Any] = _get_platform_info()
+    platform_info: dict[str, Any] = platform_info_validator.normalized(platform_info)
+    if not platform_info_validator.validate(platform_info):
+        _logger.error(f"Platform information validation failed:\n{platform_info_validator.error_str()}")
+        sys_exit(1)
+    pi_table: table = table(table_config)
+    _logger.info("Platform information: %s", str(platform_info))
+    if platform_info['signature'] not in pi_table:
+        _logger.info("New platform registered.")
+        pi_table.insert([platform_info])
+    else:
+        _logger.info("Platform already registered.")
+    _logger.info(f"Platform details:\n{pformat(platform_info, indent=4)}")
+    return platform_info
